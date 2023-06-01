f39adcb023cc3081fb36e7c43a95aa63548ece69
César <cesar.alejandro98@hotmail.com>
2023-06-01 11:09:50 +0200

    primer test

--- src/main/java/DartsGame.java
+++ src/main/java/DartsGame.java
@@ -1,5 +1,10 @@
 public class DartsGame {
 
+    public static void turno(int i) {
+    }
 
+    public static int score() {
+        return 0;
+    }
 }
 
--- src/test/java/DartsGameTest.java
+++ src/test/java/DartsGameTest.java
@@ -1,9 +1,16 @@
-import org.junit.jupiter.api.BeforeEach;
+
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class DartsGameTest {
 
-
+    @Test
+    void testTodoCero() throws Exception {
+        DartsGame dartsGame = new DartsGame();
+        for (int i = 0; i < 20; i++) {
+            DartsGame.turno(0);
+        }
+        assertEquals(0, DartsGame.score());
+    }
 }
